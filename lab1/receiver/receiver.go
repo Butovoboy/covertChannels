1177a3f3e4f4e1c45d3cada3910879b5f58622e3
Egor Malyshev <e.malyshev@group-ib.com>
2023-05-07 20:21:26 +0300

    fixed convert to ascii

--- lab1/receiver/receiver.go
+++ lab1/receiver/receiver.go
@@ -9,23 +9,23 @@ import (
 	"time"
 )
 
-func binaryToASCII(binary string) (string, error) {
+func binaryToASCII(binary string) error {
 	var ascii string
 
 	for i := 0; i < len(binary); i += 8 {
 		if i+8 > len(binary) {
-			return "", fmt.Errorf("binary string length must be a multiple of 8")
+			return fmt.Errorf("binary string length must be a multiple of 8")
 		}
 
 		value, err := strconv.ParseInt(binary[i:i+8], 2, 64)
 		if err != nil {
-			return "", err
+			return err
 		}
 
 		ascii += fmt.Sprintf("%c", value)
 	}
-
-	return ascii, nil
+	fmt.Printf(ascii)
+	return nil
 }
 
 func main() {
@@ -54,23 +54,21 @@ func main() {
 
 			zeros := int(time.Since(start).Round(time.Second).Seconds()) / 3
 			for i := 1; i < zeros; i++ {
-				fmt.Printf("0")
+				binary += fmt.Sprintf("0")
 			}
 
 			if err != nil {
 				log.Println(err)
 				continue
 			}
-			fmt.Printf("1")
+			binary += fmt.Sprintf("1")
+			if len(binary) == 8 {
+				go binaryToASCII(binary)
+				binary = ""
+			}
 			start = time.Now()
 		} else {
 			continue
 		}
-		fmt.Printf("Elapsed: %v\n", elapsed)
-		if elapsed >= 3*8*time.Second {
-			break
-		}
 	}
-	message, err := binaryToASCII(binary)
-	fmt.Println(message)
 }
