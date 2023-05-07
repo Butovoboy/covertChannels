045a7d9a61671aafba088443a8e1e2ffa1f125be
Egor Malyshev <e.malyshev@group-ib.com>
2023-05-07 21:59:38 +0300

    fixing

--- lab1/receiver/receiver.go
+++ lab1/receiver/receiver.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"net"
 	"os"
 	"strconv"
@@ -65,6 +66,7 @@ func main() {
 		if elapsed >= time.Duration(delay-10)*time.Millisecond { // 3 - is a time delay between each packet sending
 
 			zeros := int(time.Since(start).Round(time.Millisecond).Milliseconds()) / delay
+			zeros = int(math.Round(float64(zeros)/500.0)) * 500
 			fmt.Printf("ZEROZ: %v\n", int(time.Since(start).Round(time.Millisecond).Milliseconds()))
 			for i := 1; i < zeros; i++ {
 				binary = binary + "0"
